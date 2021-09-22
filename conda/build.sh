295c9804ce7b553e6e53e45181f83c27d1c05462
Taylor Denouden <taylordenouden@gmail.com>
2021-09-21 17:03:48 -0700

    Fix conda build

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,13 @@
 # Configure this build to use the PDAL and PROJ versions in the conda env
-cmake -B ./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+# shellcheck disable=SC2068
+cmake -B ./build \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DBUILD_SHARED_LIBS=ON
 
 # Build and install
-cmake --build ./build --config Release -j5
+cmake --build ./build --config Release -j"${CPU_COUNT}"
 cmake --install ./build
